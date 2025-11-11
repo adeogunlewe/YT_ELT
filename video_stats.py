548f9fe3a556adf266ca06859197dfda4d005fd0
atoki_ent-admin <adeogunlewe@gmail.com>
2025-11-10 19:04:39 -0500

    updated py script to get all video ids

--- video_stats.py
+++ video_stats.py
@@ -1,9 +1,16 @@
 import requests
 import json  
 
-API_KEY = 'AIzaSyD0kQKw_JqkaR8UyRW7q7ICywRSX4Arsc0'
+import os 
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path = "./.env")
+
+API_KEY = os.getenv("API_KEY")
 
 CHANNEL_HANDLE = "MrBeast"
+
+maxResults = 50
 
 def get_playlist_Id():
 
@@ -32,6 +39,52 @@ def get_playlist_Id():
     except requests.exceptions.RequestException as e: 
         raise e
     
+
+
+
+
+#playlist_Id = get_playlist_Id()
+
+def get_video_ids(playlistId): 
+        
+    video_ids = []
+
+    pageToken = None
+    
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+    try: 
+            
+        while True: 
+            
+            url = base_url
+
+            if pageToken: 
+                url += f"&pageToken={pageToken}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken: 
+                break 
+    
+        return video_ids
+        
+            
+    except requests.exceptions.RequestException as e: 
+            raise e
+
 if __name__ == '__main__': 
     #print("get_playlist_id will be executed")
-    get_playlist_Id()
+    playlistId = get_playlist_Id()
+    #print(get_video_ids(playlistId))
+    get_video_ids(playlistId)
