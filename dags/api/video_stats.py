a25de376f964d8415f1f02ee90723c53cf26ab54
atoki_ent-admin <adeogunlewe@gmail.com>
2025-12-28 09:46:58 -0500

    commit testing changes

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -1,21 +1,24 @@
 import requests
-import json  
+import json
 from datetime import date
 
-import os 
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
 
-load_dotenv(dotenv_path = "./.env")
+from airflow.decorators import task
+from airflow.models import Variable
 
-API_KEY = os.getenv("API_KEY")
 
-CHANNEL_HANDLE = "MrBeast"
-
+API_KEY = Variable.get("API_KEY")
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
 
-def get_playlist_Id():
 
-    try: 
+@task
+def get_playlist_id():
+
+    try:
 
         url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
@@ -23,44 +26,38 @@ def get_playlist_Id():
 
         response.raise_for_status()
 
-        print(response)
-
         data = response.json()
 
-       # print(json.dumps(data,indent = 4))
+        # print(json.dumps(data,indent=4))
 
         channel_items = data["items"][0]
 
-        channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
-
-        print(channel_playlistId)
+        channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"][
+            "uploads"
+        ]
 
         return channel_playlistId
 
-    except requests.exceptions.RequestException as e: 
+    except requests.exceptions.RequestException as e:
         raise e
-    
 
 
+@task
+def get_video_ids(playlistId):
 
-
-#playlist_Id = get_playlist_Id()
-
-def get_video_ids(playlistId): 
-        
     video_ids = []
 
     pageToken = None
-    
+
     base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
 
-    try: 
-            
-        while True: 
-            
+    try:
+
+        while True:
+
             url = base_url
 
-            if pageToken: 
+            if pageToken:
                 url += f"&pageToken={pageToken}"
 
             response = requests.get(url)
@@ -69,36 +66,31 @@ def get_video_ids(playlistId):
 
             data = response.json()
 
-            for item in data.get('items', []):
-                video_id = item['contentDetails']['videoId']
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
                 video_ids.append(video_id)
 
-            pageToken = data.get('nextPageToken')
+            pageToken = data.get("nextPageToken")
 
-            if not pageToken: 
-                break 
-    
+            if not pageToken:
+                break
+
         return video_ids
-        
-            
-    except requests.exceptions.RequestException as e: 
-            raise e
+
+    except requests.exceptions.RequestException as e:
+        raise e
 
 
-def batch_list(video_id_lst, batch_size): 
-    for video_id in range(0, len(video_id_lst), batch_size):
-        yield video_id_lst[video_id: video_id + batch_size]
+@task
+def extract_video_data(video_ids):
 
-
-def extract_video_data(video_ids): 
-    
     extracted_data = []
 
-    def batch_list(video_id_lst, batch_size): 
+    def batch_list(video_id_lst, batch_size):
         for video_id in range(0, len(video_id_lst), batch_size):
-            yield video_id_lst[video_id: video_id + batch_size]
+            yield video_id_lst[video_id : video_id + batch_size]
 
-    try: 
+    try:
         for batch in batch_list(video_ids, maxResults):
             video_ids_str = ",".join(batch)
 
@@ -110,11 +102,11 @@ def extract_video_data(video_ids):
 
             data = response.json()
 
-            for item in data.get('items',[]):
-                video_id = item['id']
-                snippet = item ['snippet']
-                contentDetails = item['contentDetails']
-                statistics = item['statistics']
+            for item in data.get("items", []):
+                video_id = item["id"]
+                snippet = item["snippet"]
+                contentDetails = item["contentDetails"]
+                statistics = item["statistics"]
 
                 video_data = {
                     "video_id": video_id,
@@ -125,26 +117,25 @@ def extract_video_data(video_ids):
                     "likeCount": statistics.get("likeCount", None),
                     "commentCount": statistics.get("commentCount", None),
                 }
-    
 
-            extracted_data.append(video_data)
+                extracted_data.append(video_data)
 
         return extracted_data
 
-    except requests.exceptions.RequestException as e: 
+    except requests.exceptions.RequestException as e:
         raise e
 
-def save_to_json(extracted_data): 
-    file_path = f"./data/YT_data{date.today()}.json"
 
-    with open(file_path, "w", encoding="utf-8") as json_outfile: 
+@task
+def save_to_json(extracted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
         json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
 
 
-if __name__ == '__main__': 
-    #print("get_playlist_id will be executed")
-    playlistId = get_playlist_Id()
-    #print(get_video_ids(playlistId))
+if __name__ == "__main__":
+    playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
     video_data = extract_video_data(video_ids)
     save_to_json(video_data)
---
+++ fernet.py
@@ -0,0 +1,7 @@
+from cryptography.fernet import Fernet
+
+# Generate a new Fernet key
+key = Fernet.generate_key()
+
+# Print the key
+print(key)
